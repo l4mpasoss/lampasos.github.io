e1f8b2b8454af0b6b0484205e271b61674a8ff64
l4mpasoss <l4mpasos@gmail.com>
2026-02-10 15:33:12 +0300

    Update start.bat

--- update/client/start.bat
+++ update/client/start.bat
@@ -3,21 +3,21 @@ setlocal
 
 REM ====== НАСТРОЙКИ ======
 set SERVER_IP=127.0.0.1
-set SERVER_PORT=7777
+set SERVER_PORT=22003
 
 REM ====== ПУТИ ======
 set BASEDIR=%~dp0
-set CRMPPATH=%BASEDIR%multiplayer_c\multiplayer_browser_cr.exe
+set MTAPATH=%BASEDIR%MTA San Andreas 1.6\Multi Theft Auto.exe
 
 echo [Launcher] Starting...
 
 REM 1) Проверка MTA в папке client\mta
 if exist "%MTAPATH%" (
   echo [Launcher] Found MTA in: %MTAPATH%
-  start "" "%CRMPPATH%"
+  start "" "%MTAPATH%"
 ) else (
   echo [Launcher] MTA not found in client\mta
-  echo [Launcher] Install MTA or put it into: %BASEDIR%crmp\
+  echo [Launcher] Install MTA or put it into: %BASEDIR%mtasa\
   pause
   exit /b 1
 )
@@ -26,6 +26,6 @@ REM 2) Пауза, чтобы MTA успел подняться
 timeout /t 2 >nul
 
 REM 3) Автоконнект (через протокол mtasa://)
-start "" "crmp://%SERVER_IP%:%SERVER_PORT%"
+start "" "mtasa://%SERVER_IP%:%SERVER_PORT%"
 
 exit /b 0
