f385a8c34e33181c2f5668ac89fc32dfafa0823e
l4mpasoss <l4mpasos@gmail.com>
2026-02-10 14:32:34 +0300

    Update start.bat

--- update/client/start.bat
+++ update/client/start.bat
@@ -3,21 +3,21 @@ setlocal
 
 REM ====== НАСТРОЙКИ ======
 set SERVER_IP=127.0.0.1
-set SERVER_PORT=22003
+set SERVER_PORT=7777
 
 REM ====== ПУТИ ======
 set BASEDIR=%~dp0
-set MTAPATH=%BASEDIR%mta\Multi Theft Auto.exe
+set CRMPPATH=%BASEDIR%multiplayer_c\multiplayer_browser_cr.exe
 
 echo [Launcher] Starting...
 
 REM 1) Проверка MTA в папке client\mta
 if exist "%MTAPATH%" (
   echo [Launcher] Found MTA in: %MTAPATH%
-  start "" "%MTAPATH%"
+  start "" "%CRMPPATH%"
 ) else (
   echo [Launcher] MTA not found in client\mta
-  echo [Launcher] Install MTA or put it into: %BASEDIR%mta\
+  echo [Launcher] Install MTA or put it into: %BASEDIR%crmp\
   pause
   exit /b 1
 )
@@ -26,6 +26,6 @@ REM 2) Пауза, чтобы MTA успел подняться
 timeout /t 2 >nul
 
 REM 3) Автоконнект (через протокол mtasa://)
-start "" "mtasa://%SERVER_IP%:%SERVER_PORT%"
+start "" "crmp://%SERVER_IP%:%SERVER_PORT%"
 
 exit /b 0
